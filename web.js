9167d21ab209dfeafdd5e1e70a4d40461ac84bbd
Luke Sandberg <lukeisandberg@gmail.com>
2013-04-02 23:08:49 -0500

    added a countdown and other tiny style tweaks

--- web.js
+++ web.js
@@ -5,6 +5,16 @@ var express = require('express');
 var utils = {
 	getDriveImageUrl: function(filename) {
 		return 'https://googledrive.com/host/0B6vNk5GSBIjUWG9xNklsYnVGaHc/' + filename;
+	},
+	getDaysToGo: function() {
+		var daysLeft = Math.round((new Date(2013, 6, 13) - new Date()) / (1000*60*60*24));
+		if (daysLeft < 0) {
+			return "";
+		}
+		if (daysLeft == 0 || daysLeft == 1) {
+			return "The Wedding is Soon!";
+		}
+		return daysLeft + " days to go!";
 	}
 };
 
@@ -22,10 +32,19 @@ app.configure(function()  {
 });
 
 app.get('/', function(request, response) {
-	response.render('home.html', {banner: utils.getDriveImageUrl('homepage_banner.jpg'), active: 'Home'});
+	console.log(utils.getDaysToGo());
+	response.render('home.html', {
+		banner: utils.getDriveImageUrl('homepage_banner.jpg'), 
+		active: 'Home', 
+		daysToGo: utils.getDaysToGo()
+	});
 });
 app.get('/bride-and-groom/', function(request, response) {
-	response.render('bride_and_groom.html', {banner: utils.getDriveImageUrl('bride_and_groom_banner.jpg'), active: 'Bride & Groom'});
+	response.render('bride_and_groom.html', {
+		banner: utils.getDriveImageUrl('bride_and_groom_banner.jpg'), 
+		active: 'Bride & Groom',
+		daysToGo: utils.getDaysToGo()
+	});
 });
 
 var port = process.env.PORT || 5000;
